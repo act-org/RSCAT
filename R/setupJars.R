7a74f48b43ae79dff5fafaf0b0ed5dd910a222b3
Bingnan Jiang <bnjiangece@gmail.com>
2021-05-06 16:49:34 -0600

    Updated java path and jar resource urls

--- R/setupJars.R
+++ R/setupJars.R
@@ -10,16 +10,16 @@
 #' @export
 setupJars <- function() {
   javaPath <- system.file("java", package = "RSCAT")
-  download.file("http://central.maven.org/maven2/commons-io/commons-io/2.2/commons-io-2.2.jar", 
-                paste(javaPath, "commons-io-2.2.jar", sep = "/"), mode = "wb")
-  download.file("http://central.maven.org/maven2/org/apache/commons/commons-lang3/3.6/commons-lang3-3.6.jar", 
-                paste(javaPath, "commons-lang3-3.6.jar", sep = "/"), mode = "wb")
-  download.file("http://central.maven.org/maven2/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar", 
+  download.file("https://repo1.maven.org/maven2/commons-io/commons-io/2.6/commons-io-2.6.jar", 
+                paste(javaPath, "commons-io-2.6.jar", sep = "/"), mode = "wb")
+  download.file("https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.8.1/commons-lang3-3.8.1.jar", 
+                paste(javaPath, "commons-lang3-3.8.1.jar", sep = "/"), mode = "wb")
+  download.file("https://repo1.maven.org/maven2/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar", 
                 paste(javaPath, "commons-math3-3.6.1.jar", sep = "/"), mode = "wb")
-  download.file("http://central.maven.org/maven2/log4j/log4j/1.2.17/log4j-1.2.17.jar", 
+  download.file("https://repo1.maven.org/maven2/log4j/log4j/1.2.17/log4j-1.2.17.jar", 
                 paste(javaPath, "log4j-1.2.17.jar", sep = "/"), mode = "wb")
-  download.file("http://central.maven.org/maven2/org/slf4j/slf4j-api/1.7.21/slf4j-api-1.7.21.jar", 
-                paste(javaPath, "slf4j-api-1.7.21.jar", sep = "/"), mode = "wb")
-  download.file("http://central.maven.org/maven2/org/slf4j/slf4j-log4j12/1.7.21/slf4j-log4j12-1.7.21.jar", 
-                paste(javaPath, "slf4j-log4j12-1.7.21.jar", sep = "/"), mode = "wb")
+  download.file("https://repo1.maven.org/maven2/org/slf4j/slf4j-api/1.7.26/slf4j-api-1.7.26.jar", 
+                paste(javaPath, "slf4j-api-1.7.26.jar", sep = "/"), mode = "wb")
+  download.file("https://repo1.maven.org/maven2/org/slf4j/slf4j-log4j12/1.7.26/slf4j-log4j12-1.7.26.jar", 
+                paste(javaPath, "slf4j-log4j12-1.7.26.jar", sep = "/"), mode = "wb")
 }
