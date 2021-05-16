f8e9cb8bfeae3760f83ab792c59c67fbf87c7205
Bingnan Jiang <bnjiangece@gmail.com>
2021-05-15 20:13:52 -0600

    Updated the commons-io version.

--- R/setupJars.R
+++ R/setupJars.R
@@ -10,8 +10,8 @@
 #' @export
 setupJars <- function() {
   javaPath <- system.file("java", package = "RSCAT")
-  download.file("https://repo1.maven.org/maven2/commons-io/commons-io/2.6/commons-io-2.6.jar", 
-                paste(javaPath, "commons-io-2.6.jar", sep = "/"), mode = "wb")
+  download.file("https://repo1.maven.org/maven2/commons-io/commons-io/2.7/commons-io-2.7.jar", 
+                paste(javaPath, "commons-io-2.7.jar", sep = "/"), mode = "wb")
   download.file("https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.8.1/commons-lang3-3.8.1.jar", 
                 paste(javaPath, "commons-lang3-3.8.1.jar", sep = "/"), mode = "wb")
   download.file("https://repo1.maven.org/maven2/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar", 
